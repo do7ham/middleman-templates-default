0703ee551da1df8378671ffbfe79493e3997b61e
Mathias Davidheimann <info@do7ham.de>
2018-02-04 03:07:41 +0100

    Create asset_helpers.rb

---
+++ template/helpers/asset_helpers.rb
@@ -0,0 +1,14 @@
+# *************************************
+#  Asset Helpers
+# *************************************
+
+module AssetHelpers
+  # ???
+  # =============================
+  def inline_stylesheet(name) do
+  end
+  # ???
+  # =============================
+  def inline_javascript(name) do
+  end
+end
